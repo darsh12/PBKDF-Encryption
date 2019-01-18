23084039b91fb8ae67522121dda11848f4d9c055
Darshan Patel <darshan7230@gmail.com>
2019-01-17 16:25:04 -0800

    Move pbkdf and encryption function to a different package

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
+	"CSS577/functions"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/hmac"
-	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
-	"golang.org/x/crypto/pbkdf2"
-	"io"
 )
 
 var HASHALGORITHM = sha256.New
@@ -18,54 +16,13 @@ const KEYLENGTH = 32
 
 func main() {
 	//fmt.Print("hello world")
-	masterKey := pbkdf2.Key([]byte("hey"), []byte("yeh"), 1000000, KEYLENGTH, HASHALGORITHM)
-
-	encryptionKey := pbkdf2.Key(masterKey, []byte("encryption"), 1, KEYLENGTH, HASHALGORITHM)
-
-	hmacKey := pbkdf2.Key(encryptionKey, []byte("hmac"), 1, KEYLENGTH, HASHALGORITHM)
+	_, encryptionKey, hmacKey := helper.Pbkdf([]byte("hey"), []byte("yeh"), 1000000, KEYLENGTH, HASHALGORITHM)
 
 	//fmt.Printf("%x\n%x\n%x\n", masterKey, encryptionKey, hmacKey)
 
-	plainText := []byte("exampleplaintext")
+	plainText := "exampleplaintext"
 
-	//Assume the plainText is a multiple if a block size
-	if len(plainText)%aes.BlockSize != 0 {
-		panic("Not a multiple")
-	}
-
-	//Create a new block
-	block, err := aes.NewCipher(encryptionKey)
-
-	if err != nil {
-		panic(err)
-	}
-
-	//Create a random iv and attach it at the start of the cipherText
-
-	cipherText := make([]byte, aes.BlockSize+len(plainText))
-
-	//Create a byte array of the block size
-	iv := cipherText[:aes.BlockSize]
-
-	//Use the readfull func to copy exact bytes to the buffer
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
-	}
-
-	//Use the cbc mode, and start appending the encryptedtext at the end of cipherText
-	mode := cipher.NewCBCEncrypter(block, iv)
-	mode.CryptBlocks(cipherText[aes.BlockSize:], plainText)
-
-	//Create a new hmac block
-	hmacBlock := hmac.New(HASHALGORITHM, hmacKey)
-	//Write data to the block
-	hmacBlock.Write(cipherText)
-
-	//Get the actual hash of the cipherText
-	hmacSum := hmacBlock.Sum(nil)
-
-	fmt.Printf("Cipher Text: %x\n", cipherText)
-	fmt.Printf("HMAC Hash: %x\n", hmacSum)
+	cipherText, hmacSum := helper.Encryption(plainText, encryptionKey, hmacKey, HASHALGORITHM)
 
 	//Create a hmac block
 	decryptHmacBlock := hmac.New(HASHALGORITHM, hmacKey)
